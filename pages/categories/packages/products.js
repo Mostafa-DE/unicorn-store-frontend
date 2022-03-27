0ba692f6eade2373e33f25a502f32d11a9853bce
mostafa-de <mostafa@celtisaustralis.com>
2022-03-27 22:19:02 +0300

    1) add pagination for packages collection pages.

--- pages/categories/packages/products.js
+++ pages/categories/packages/products.js
@@ -1,45 +1,62 @@
-import { API_URL } from "@/config/index";
+import {API_URL} from "@/config/index";
 import Layout from "@/components/Layout/Layout";
 import SearchInput from "@/components/SearchInput/SearchInput";
 import ProductsWithSearch from "@/components/ProductsWithSearch/ProductsWithSearch";
-import { parseCookies } from "@/helpers/index";
-import { AiOutlineLine } from "react-icons/ai";
+import {parseCookies} from "@/helpers/index";
+import {AiOutlineLine} from "react-icons/ai";
 import useSearch from "@/Hooks/useSearch";
+import {getStartAndEndValueForPagination, values} from "@/helpers/paginationLogic";
+import Pagination from "@/components/Pagination"
+import usePagination from "@/Hooks/usePagination"
 
-export default function Packages({ packages, token }) {
-  const pathname = "/categories/packages";
-  const [searchTerm, handleChange] = useSearch("");
+export default function Packages({packages, token, totalPages}) {
+    const pathname = "/categories/packages";
+    const [searchTerm, handleChange] = useSearch("");
+    const [page, handleChangePage] = usePagination(1);
 
-  return (
-    <Layout title="Packages & Gifts & More">
-      <div data-aos="fade-in" className="containerTitle">
-        <h1 className="h1Title"> قسم الهدايا والباكيجات</h1>
-        <AiOutlineLine className="lineIcon" />
-      </div>
+    return (
+        <Layout title="Packages & Gifts & More">
+            <div data-aos="fade-in"
+                 className="containerTitle"
+            >
+                <h1 className="h1Title"> قسم الهدايا والباكيجات</h1>
+                <AiOutlineLine className="lineIcon"/>
+            </div>
 
-      <SearchInput searchTerm={searchTerm} handleChange={handleChange} />
-      <div className="containerCardProducts">
-        <ProductsWithSearch
-          productsData={packages}
-          pathname={pathname}
-          searchTerm={searchTerm}
-          token={token}
-        />
-      </div>
-    </Layout>
-  );
+            <SearchInput searchTerm={searchTerm}
+                         handleChange={handleChange}
+            />
+            <div className="containerCardProducts">
+                <ProductsWithSearch
+                    productsData={packages}
+                    pathname={pathname}
+                    searchTerm={searchTerm}
+                    token={token}
+                />
+            </div>
+            {packages.length !== 0 && (
+                <Pagination
+                    page={page}
+                    totalPages={totalPages}
+                    handleChangePage={handleChangePage}
+                />
+            )}
+        </Layout>
+    );
 }
 
-export async function getServerSideProps({ req }) {
-  const { token = null } = parseCookies(req);
-  const res = await fetch(`${API_URL}/packages`);
+export async function getServerSideProps({req, query: {page = 1}}) {
+    const {token = null} = parseCookies(req);
+    const res = await fetch(`${API_URL}/packages`);
 
-  const packages = await res.json();
+    const packages = await res.json();
+    getStartAndEndValueForPagination(packages, page);
 
-  return {
-    props: {
-      packages: packages,
-      token: token
-    }
-  };
+    return {
+        props: {
+            packages: packages.slice(values.start, values.end),
+            token: token,
+            totalPages: values.totalPages
+        }
+    };
 }
