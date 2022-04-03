749329f3075eb13c0834dbdd6e80d573db0cff80
mostafa-de <mostafa@celtisaustralis.com>
2022-04-04 01:46:28 +0300

    add reviews section in kids fashions pages

--- pages/categories/kids-fashions/all-products/[slug].js
+++ pages/categories/kids-fashions/all-products/[slug].js
@@ -2,8 +2,9 @@ import Layout from "@/components/Layout/Layout";
 import {API_URL} from "@/config/index";
 import ProductDetails from "@/components/ProductDetails/ProductDetails";
 import {parseCookies} from "@/helpers/index";
+import qs from "qs";
 
-export default function ProductDetailsPage({product, token}) {
+export default function ProductDetailsPage({product, token, reviews}) {
     return (
         <Layout>
             {product.map((product) => (
@@ -11,6 +12,7 @@ export default function ProductDetailsPage({product, token}) {
                     token={token}
                     key={product.id}
                     product={product}
+                    reviews={reviews}
                 />
             ))}
         </Layout>
@@ -18,15 +20,25 @@ export default function ProductDetailsPage({product, token}) {
 }
 
 export async function getServerSideProps({req, query: {slug}}) {
-    const { token = null } = parseCookies(req)
+    const {token = null} = parseCookies(req)
 
     const res = await fetch(`${API_URL}/kids-products?slug=${slug}`);
     const product = await res.json();
 
+    const query = qs.stringify({
+        _where: [
+            {product: `/categories/kids-fashions/all-products/${slug}`},
+        ],
+    });
+
+    const resReviews = await fetch(`${API_URL}/reviews?${query}`)
+    const reviews = await resReviews.json()
+
     return {
         props: {
             product,
-            token
+            token,
+            reviews
         },
     };
 }
