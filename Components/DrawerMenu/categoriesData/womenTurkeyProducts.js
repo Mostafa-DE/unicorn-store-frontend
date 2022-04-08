83f0917567624b8220d6edafa9efc9758ce1f408
mostafa-de <mostafa@celtisaustralis.com>
2022-04-08 18:42:27 +0300

    fix the links in the drawer

--- Components/DrawerMenu/categoriesData/womenTurkeyProducts.js
+++ Components/DrawerMenu/categoriesData/womenTurkeyProducts.js
@@ -4,28 +4,6 @@ export const WomenCategoriesProducts = [
         productsData: [
             {
                 title: "فساتين سهرة",
-                link: "/categories/women-fashions/local-dresses/dresses"
-            },
-            {
-                title: "ﻻنجري",
-                link: "/categories/women-fashions/local-lingeries/lingerie"
-            },
-            {
-                title: "عبايات و قطافين",
-                link: "/categories/women-fashions/local-abayas/abaya"
-            },
-            {
-                title: "جميع المنتجات",
-                link: "/categories/women-fashions/local-all-products/other-products"
-            },
-        ],
-
-    },
-    {
-        title: "المنتجات المحلية",
-        productsData: [
-            {
-                title: "فساتين سهرة",
                 link: "/categories/women-fashions/turkey-dresses/dresses"
             },
             {
@@ -39,6 +17,28 @@ export const WomenCategoriesProducts = [
             {
                 title: "جميع المنتجات",
                 link: "/categories/women-fashions/turkey-all-products/other-products"
+            },
+        ],
+
+    },
+    {
+        title: "المنتجات المحلية",
+        productsData: [
+            {
+                title: "فساتين سهرة",
+                link: "/categories/women-fashions/local-dresses/dresses"
+            },
+            {
+                title: "ﻻنجري",
+                link: "/categories/women-fashions/local-lingeries/lingerie"
+            },
+            {
+                title: "عبايات و قطافين",
+                link: "/categories/women-fashions/local-abayas/abaya"
+            },
+            {
+                title: "جميع المنتجات",
+                link: "/categories/women-fashions/local-all-products/other-products"
             },
         ],
 
