3cf61c1b41236d548d5feb5010f13625f1d70bab
mostafa-de <mostafa@celtisaustralis.com>
2022-05-02 08:54:53 +0300

    fix origin header block when we added a new domain name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+    reactStrictMode: true,
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+    },
 });
+
+module.exports = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    {key: "Access-Control-Allow-Credentials", value: "true"},
+                    {key: "Access-Control-Allow-Origin", value: "*"},
+                    {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
+                    {key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"},
+                ]
+            }
+        ]
+    }
+};
