0141bafd1fd481f2ade17401a966345847e7ef58
mostafa-de <mostafa@celtisaustralis.com>
2022-05-29 23:36:37 +0300

    update packages and fix nprogress warning

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -55,13 +55,6 @@ export default function Layout({title, description, children, userAccount}) {
                 <link rel="icon"
                       href="/images/unicorn.png"
                 />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-                    integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-                    crossOrigin="anonymous"
-                    referrerPolicy="no-referrer"
-                />
             </Head>
             <Header/>
             {router.pathname === "/" && (
--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,14 @@ class MyDocument extends Document {
                     <meta name="theme-color"
                           content="#fb9aa7"
                     />
+
+                    <link
+                        rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                        integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+                        crossOrigin="anonymous"
+                        referrerPolicy="no-referrer"
+                    />
                 </Head>
                 <body>
                 <Main/>
